6545eb77a769942dc9a2dbc829b72f095bf96129
Kenny To <knyto2@gmail.com>
2020-08-12 12:08:19 -0700

    test

--- husky.config.js
+++ husky.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   husky: {
     hooks: {
-      'pre-commit': 'lint-staged',
+      'pre-commit': 'echo test',
+      // 'pre-commit': 'npm run pretty-quick && npm run lint',
     },
   },
 };
